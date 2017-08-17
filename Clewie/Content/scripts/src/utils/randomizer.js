e7b31ea8e81cf91484144d3573471ac84fb84cda
jgom <jost.gombac@ineor.si>
2017-08-17 17:40:58 +0200

    ..

---
+++ Clewie/Content/scripts/src/utils/randomizer.js
@@ -0,0 +1,17 @@
+﻿var gom = gom || {};
+
+gom.randomizer = {
+
+    getNumber: function (min, max) {
+
+    },
+
+    getString: function (len) {
+        var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        var result = "";
+        for (var i = 0; i < len; i++) {
+            result += characters.charAt(Math.floor(Math.random() * characters.length));
+        }
+        return result;
+    },
+}
