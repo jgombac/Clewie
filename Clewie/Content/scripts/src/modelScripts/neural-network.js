78fb203e6006f848f185c8fb81de107a3f0b2f03
jgom <jost.gombac@ineor.si>
2017-08-30 18:59:08 +0200

    ..

--- Clewie/Content/scripts/src/modelScripts/neural-network.js
+++ Clewie/Content/scripts/src/modelScripts/neural-network.js
@@ -38,10 +38,10 @@
 gom.neuralNetwork = {
 
     init: function () {
-        var nodes = gom.neuralNetwork.layersToJson([3, 4, 6, 10, 10, 2]);
+        var nodes = gom.neuralNetwork.layersToJson([1, 3, 4, 6, 10, 10, 2]);
 
         // set the dimensions and margins of the diagram
-        var margin = { top: 40, right: 90, bottom: 50, left: 90 },
+        var margin = { top: 50, right: 90, bottom: 50, left: 90 },
             width = $("#layer-designer").width() - margin.left - margin.right,
             height = $("#layer-designer").height() - margin.top - margin.bottom;
         
@@ -63,18 +63,31 @@ gom.neuralNetwork = {
         node.append("circle")
             .attr("r", 10);
 
+        var link = g.selectAll(".link")
+            .data(nodes.slice(1))
+            .enter().append("path")
+            .attr("class", "link")
+            .attr("d", function (d) {
+                return "M" + d.x + "," + d.y
+                    + "C" + d.x + "," + (d.y + d.parent.y) / 2
+                    + " " + d.parent.x + "," + (d.y + d.parent.y) / 2
+                    + " " + d.parent.x + "," + d.parent.y;
+            });
 
     },
 
-    layersToJson: function (layers) {
+    layersToNodes: function (layers) {
         var arr = [],
+            vCenter = $("#layer-designer").height(); // 2;
             x = 0,
             y = 0,
-            mult = 20;
+            mult = 40;
 
-        for (var i = 0; i < layers.length; i++) {
-            for (var j = 0; j < layers[i]; j++) {
-                arr.push({ "name": i + " " + j, x: x, y: y });
+            for (var i = 0; i < layers.length; i++) {
+                y = Math.round((vCenter - layers[i] * mult) / 2);
+                console.log(y, vCenter, layers[i]);
+                for (var j = 0; j < layers[i]; j++) {
+                    arr.push({ "name": i + " " + j, x: x, y: y, desc: [{ function(d) { d.selectAll(); } }] });
                 y = y + mult
             }
             y = 0;
@@ -82,6 +95,16 @@ gom.neuralNetwork = {
         }
 
         return arr;
-    }
+    },
+
+    layersToLinks: function (layers) {
+        arr = [];
+
+
+    },
+
+    //3
+
+    
 
 }
